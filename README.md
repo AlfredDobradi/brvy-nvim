e09586afb0f80bc478fe3a795d65a7401cbf67fa
Alfred Dobradi <alfreddobradi@gmail.com>
2026-05-01 00:16:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# brvy-nvim
+
+Yet another Neovim config
+
+## Packages so far
+
+* [catppuccin-nvim](https://github.com/catppuccin/nvim)
+* [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
+* [mason](https://github.com/mason-org/mason.nvim)
+* [mason-lspconfig](https://github.com/mason-org/mason-lspconfig.nvim)
+* [ctrlp](https://github.com/ctrlpvim/ctrlp.vim)
+* [plenary](https://github.com/nvim-lua/plenary.nvim)
+* [telescope](https://github.com/nvim-telescope/telescope.nvim)
+* [nvim-cmp](https://github.com/hrsh7th/nvim-cmp)
+* [cmp-nvim-lsp](https://github.com/hrsh7th/cmp-nvim-lsp)
+* [nvim-tree](https://github.com/nvim-tree/nvim-web-devicons)
+* [which-key](https://github.com/folke/which-key.nvim)
+* [lir](https://github.com/tamago324/lir.nvim)
+* [project](https://github.com/ahmedkhalf/project.nvim)
+* [lualine](https://github.com/nvim-lualine/lualine.nvim)
---
+++ init.lua
@@ -0,0 +1,63 @@
+vim.g.mapleader = ","
+
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.python3_host_prog = "/Users/brvy/.local/bin/pynvim-python"
+
+vim.opt.tabstop = 4
+vim.opt.expandtab = true        
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.number = true
+
+-- Keymaps
+vim.keymap.set('n', '<Leader>r', ':luafile $MYVIMRC<CR>')
+
+-- Theme related
+vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+
+require("catppuccin").setup({
+    flavour = "latte",
+})
+
+vim.cmd.colorscheme "catppuccin-nvim"
+
+--- Plenary
+vim.pack.add { { src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary.nvim" } }
+
+--- Telescope
+vim.pack.add { { src = "https://github.com/nvim-telescope/telescope.nvim", name = "telescope.nvim" } }
+
+--- Mason
+vim.pack.add { { src = "https://github.com/mason-org/mason.nvim", name = "mason.nvim" } }
+vim.pack.add { { src = "https://github.com/neovim/nvim-lspconfig", name = "nvim-lspconfig" } }
+vim.pack.add { { src = "https://github.com/mason-org/mason-lspconfig.nvim" } }
+
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+--- CtrlP
+vim.pack.add { { src = "https://github.com/ctrlpvim/ctrlp.vim" } }
+
+--- CMP
+vim.pack.add { { src = "https://github.com/hrsh7th/nvim-cmp" } }
+vim.pack.add { { src = "https://github.com/hrsh7th/cmp-nvim-lsp" } }
+
+--- WhichKey
+vim.pack.add { { src = "https://github.com/nvim-tree/nvim-web-devicons" } }
+vim.pack.add { { src = "https://github.com/folke/which-key.nvim" } }
+local wk = require("which-key")
+wk.add({
+    { "<leader>?", function() wk.show() end },
+})
+
+--- Files
+vim.pack.add { { src = "https://github.com/kyazdani42/nvim-tree.lua" } }
+vim.pack.add { { src = "https://github.com/tamago324/lir.nvim" } }
+
+-- --- Project
+vim.pack.add { { src = "https://github.com/ahmedkhalf/project.nvim" } }
+
+-- --- Status line
+vim.pack.add { { src = "https://github.com/nvim-lualine/lualine.nvim" } }
+require('lualine').setup()
